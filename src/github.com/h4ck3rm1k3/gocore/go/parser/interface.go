f9d2583a38ec437805c6cd28ce7a5b47fd8e7fac
James Michael DuPont <jamesmikedupont@googlemail.com>
2015-04-02 12:49:51 +0000

    * .gitignore:

--- src/github.com/h4ck3rm1k3/gocore/go/parser/interface.go
+++ src/github.com/h4ck3rm1k3/gocore/go/parser/interface.go
@@ -81,8 +81,6 @@ const (
 // representing the fragments of erroneous source code). Multiple errors
 // are returned via a scanner.ErrorList which is sorted by file position.
 //
-func ParseFile2(fset *token.FileSet) {
-}
 	
 func ParseFile(fset *token.FileSet, filename string, src interface{}, mode Mode) (f *ast.File, err error) {
 	// get source
