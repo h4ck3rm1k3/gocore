a30efeea19554c71f3daccf2c03f0d98e9428565
James Michael DuPont <jamesmikedupont@googlemail.com>
2015-04-04 17:41:32 +0000

    fake values
    adding in fake stubs for atomic values to avoid compiling
    * .gitignore:

--- src/github.com/h4ck3rm1k3/gocore/sync/atomic/value.go
+++ src/github.com/h4ck3rm1k3/gocore/sync/atomic/value.go
@@ -23,6 +23,7 @@ type ifaceWords struct {
 	data unsafe.Pointer
 }
 
+
 // Load returns the value set by the most recent Store.
 // It returns nil if there has been no call to Store for this Value.
 func (v *Value) Load() (x interface{}) {
@@ -38,6 +39,7 @@ func (v *Value) Load() (x interface{}) {
 	xp.data = data
 	return
 }
+
 
 // Store sets the value of the Value to x.
 // All calls to Store for a given Value must use values of the same concrete type.
@@ -84,3 +86,85 @@ func (v *Value) Store(x interface{}) {
 // Disable/enable preemption, implemented in runtime.
 func runtime_procPin()
 func runtime_procUnpin()
+
+
+
+// hack
+func LoadPointer(addr *unsafe.Pointer) (val unsafe.Pointer) {
+	return *addr
+
+}
+func runtime_procPin() {}
+func runtime_procUnpin() {}
+func StorePointer(addr *unsafe.Pointer, val unsafe.Pointer) {
+
+}
+func CompareAndSwapPointer(addr *unsafe.Pointer, old, new unsafe.Pointer) (swapped bool) {
+	return false
+}
+
+func AddInt64(addr *int64, delta int64) (new int64) {
+	return *addr
+}
+
+func SwapUint64(addr *uint64, new uint64) (old uint64) {return new}
+
+// SwapUintptr atomically stores new into *addr and returns the previous *addr value.
+func SwapUintptr(addr *uintptr, new uintptr) (old uintptr) {return new}
+
+func SwapUint32(addr *uint32, new uint32) (old uint32) {return new}
+
+func LoadInt32(addr *int32) (val int32) {return *addr}
+
+// LoadInt64 atomically loads *addr.
+func LoadInt64(addr *int64) (val int64) {return *addr}
+
+// LoadUint32 atomically loads *addr.
+func LoadUint32(addr *uint32) (val uint32) {return *addr}
+
+// LoadUint64 atomically loads *addr.
+func LoadUint64(addr *uint64) (val uint64) {return *addr}
+
+func AddInt32(addr *int32, delta int32) (new int32) {return delta}
+func AddUint32(addr *uint32, delta uint32) (new uint32) {return delta}
+
+func AddUint64(addr *uint64, delta uint64) (new uint64) {return delta}
+func AddUintptr(addr *uintptr, delta uintptr) (new uintptr) {return delta}
+
+// CompareAndSwapInt32 executes the compare-and-swap operation for an int32 value.
+func CompareAndSwapInt32(addr *int32, old, new int32) (swapped bool) {return false}
+
+// CompareAndSwapInt64 executes the compare-and-swap operation for an int64 value.
+func CompareAndSwapInt64(addr *int64, old, new int64) (swapped bool) {return false}
+
+// CompareAndSwapUint32 executes the compare-and-swap operation for a uint32 value.
+func CompareAndSwapUint32(addr *uint32, old, new uint32) (swapped bool) {return false}
+
+// CompareAndSwapUint64 executes the compare-and-swap operation for a uint64 value.
+func CompareAndSwapUint64(addr *uint64, old, new uint64) (swapped bool) {return false}
+
+// CompareAndSwapUintptr executes the compare-and-swap operation for a uintptr value.
+func CompareAndSwapUintptr(addr *uintptr, old, new uintptr) (swapped bool) {return false}
+
+func LoadUintptr(addr *uintptr) (val uintptr) { return * addr}
+
+func StoreInt32(addr *int32, val int32){}
+
+// StoreInt64 atomically stores val into *addr.
+func StoreInt64(addr *int64, val int64){}
+
+// StoreUint32 atomically stores val into *addr.
+func StoreUint32(addr *uint32, val uint32){}
+
+// StoreUint64 atomically stores val into *addr.
+func StoreUint64(addr *uint64, val uint64){}
+
+func SwapInt32(addr *int32, new int32) (old int32){return new}
+
+// SwapInt64 atomically stores new into *addr and returns the previous *addr value.
+func SwapInt64(addr *int64, new int64) (old int64){return new}
+func SwapPointer(addr *unsafe.Pointer, new unsafe.Pointer) (old unsafe.Pointer) { return new} 
+
+
+
+func StoreUintptr(addr *uintptr, val uintptr) {}
