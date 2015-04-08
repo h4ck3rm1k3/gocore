24eadb3ba86530dd4890fd69616bff21d60bab5b
James Michael DuPont <jamesmikedupont@googlemail.com>
2015-04-08 00:47:19 +0000

    adding more function calls
    * src/github.com/h4ck3rm1k3/gocore/sync/atomic/value.go:

--- src/github.com/h4ck3rm1k3/gocore/sync/atomic/value.go
+++ src/github.com/h4ck3rm1k3/gocore/sync/atomic/value.go
@@ -5,7 +5,7 @@
 package atomic
 
 import (
-	//"unsafe"
+	//"runtime"
 	"unsafe"
 	rtatom "sync/atomic"
 )
@@ -89,135 +89,136 @@ func (v *Value) Store(x interface{}) {
 func run_time_procPin() {panic("undef")}
 func run_time_procUnpin() {panic("undef")}
 
-
-
 // hack
 func LoadPointer(addr *unsafe.Pointer) (val unsafe.Pointer) {
-	return *addr
-
+	return rtatom.LoadPointer(addr)
 }
 
 //OAfunc run_time_procUnpin() { panic("todo") }
 func StorePointer(addr *unsafe.Pointer, val unsafe.Pointer) {
-panic("todo") 
+	rtatom.StorePointer(addr, val)
+
 }
 func CompareAndSwapPointer(addr *unsafe.Pointer, old, new unsafe.Pointer) (swapped bool) {
-	panic("todo") 
-	return false
+	return rtatom.CompareAndSwapPointer(addr, old, new)
 }
 
 func AddInt64(addr *int64, delta int64) (new int64) {
-	panic("todo") 
-	return *addr
+	return rtatom.AddInt64(addr, delta)
 }
 
 func SwapUint64(addr *uint64, new uint64) (old uint64) {
-	panic("todo") 
-	return new}
+	return rtatom.SwapUint64(addr, new)
+}
 
 // SwapUintptr atomically stores new into *addr and returns the previous *addr value.
-func SwapUintptr(addr *uintptr, new uintptr) (old uintptr) {panic("todo")
-	return new}
+func SwapUintptr(addr *uintptr, new uintptr) (old uintptr) {
+	return rtatom.SwapUintptr(addr, new)
+}
 
 func SwapUint32(addr *uint32, new uint32) (old uint32) {
-panic("todo") 
-	return new}
+	return rtatom.SwapUint32(addr, new)
+}
 
 func LoadInt32(addr *int32) (val int32) {
+	//return rtatom.SwapUint32(addr, new)
 	return rtatom.LoadInt32(addr)
 }
 
 // LoadInt64 atomically loads *addr.
 func LoadInt64(addr *int64) (val int64) {
-panic("todo") 
-	return *addr}
+	return rtatom.LoadInt64(addr)
+}
 
 // LoadUint32 atomically loads *addr.
 func LoadUint32(addr *uint32) (val uint32) {
 	return rtatom.LoadUint32(addr)
-	}
+}
 
 // LoadUint64 atomically loads *addr.
 func LoadUint64(addr *uint64) (val uint64) {
-	panic("todo") 
-	return *addr}
+	return rtatom.LoadUint64(addr)
+}
 
 func AddInt32(addr *int32, delta int32) (new int32) {
-panic("todo") 
-	return delta}
+	return rtatom.AddInt32(addr, delta)
+}
 func AddUint32(addr *uint32, delta uint32) (new uint32) {
-panic("todo") 
-	return delta}
+	return rtatom.AddUint32(addr, delta)
+}
 
 func AddUint64(addr *uint64, delta uint64) (new uint64) {
-panic("todo") 
-	return delta}
+	return rtatom.AddUint64(addr,delta)
+}
 func AddUintptr(addr *uintptr, delta uintptr) (new uintptr) {
-
-panic("todo") 
-	return delta}
+	return rtatom.AddUintptr(addr, delta)
+}
 
 // CompareAndSwapInt32 executes the compare-and-swap operation for an int32 value.
 func CompareAndSwapInt32(addr *int32, old, new int32) (swapped bool) {
-panic("todo") 
-	return false}
+	return rtatom.CompareAndSwapInt32(addr,old,new)
+}
 
 // CompareAndSwapInt64 executes the compare-and-swap operation for an int64 value.
 func CompareAndSwapInt64(addr *int64, old, new int64) (swapped bool) {
-panic("todo") 
-	return false}
+	return rtatom.CompareAndSwapInt64(addr,old, new)
+}
 
 // CompareAndSwapUint32 executes the compare-and-swap operation for a uint32 value.
 func CompareAndSwapUint32(addr *uint32, old, new uint32) (swapped bool) {
-panic("todo") 
-	return false}
+	return rtatom.CompareAndSwapUint32(addr, old, new)
+}
 
 // CompareAndSwapUint64 executes the compare-and-swap operation for a uint64 value.
 func CompareAndSwapUint64(addr *uint64, old, new uint64) (swapped bool) {
-panic("todo") 
-	return false}
+	return rtatom.CompareAndSwapUint64(addr, old, new)
+}
 
 // CompareAndSwapUintptr executes the compare-and-swap operation for a uintptr value.
 func CompareAndSwapUintptr(addr *uintptr, old, new uintptr) (swapped bool) {
-panic("todo") 
-	return false}
+	return rtatom.CompareAndSwapUintptr(addr, old, new)
+}
 
-func LoadUintptr(addr *uintptr) (val uintptr) { panic("todo")
-	return * addr}
+func LoadUintptr(addr *uintptr) (val uintptr) { 
+	return rtatom.LoadUintptr(addr)
+}
 
 func StoreInt32(addr *int32, val int32){
-panic("todo") 
+	rtatom.StoreInt32(addr,val)
 }
 
 // StoreInt64 atomically stores val into *addr.
 func StoreInt64(addr *int64, val int64){
-panic("todo") 
+	rtatom.StoreInt64(addr, val)
+
 }
 
 // StoreUint32 atomically stores val into *addr.
 func StoreUint32(addr *uint32, val uint32){
-panic("todo") 
+	rtatom.StoreUint32(addr, val)
+
 }
 
 // StoreUint64 atomically stores val into *addr.
 func StoreUint64(addr *uint64, val uint64){
-panic("todo") 
+	rtatom.StoreUint64(addr,val)
 }
 
 func SwapInt32(addr *int32, new int32) (old int32){
-panic("todo") 
-	return new}
+	return rtatom.SwapInt32(addr,new)
+}
 
 // SwapInt64 atomically stores new into *addr and returns the previous *addr value.
 func SwapInt64(addr *int64, new int64) (old int64){
-panic("todo") 
-	return new}
+	return rtatom.SwapInt64(addr, new)
+}
+
 func SwapPointer(addr *unsafe.Pointer, new unsafe.Pointer) (old unsafe.Pointer) {
-panic("todo") 
-	return new} 
+	return rtatom.SwapPointer(addr, new)
+} 
 
 
 
 func StoreUintptr(addr *uintptr, val uintptr) {
-panic("todo") 
+	rtatom.StoreUintptr(addr, val)
 }
